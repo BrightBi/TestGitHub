3f90b2156c30cc622dc654d16c757191a7bb9197
mbi <mingliang.bi@activenetwork.com>
2017-04-10 17:31:38 +0800

    Update

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,15 +3,11 @@ package test;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("0000");
-        System.out.println("0011");
-        System.out.println("0022");
-        System.out.println("0033");
         System.out.println("0044");
+        System.out.println("0000");
     }
     
     public void printer(String info) {
-        System.out.println(info);
         System.out.println(info);
     }
 }
